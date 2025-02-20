f40bc6b1bb1f2e1c82f0c1dbaf4ec16ee678c03d
havid424 <havid.10424@mhs.amikomsolo.ac.id>
2025-02-20 15:11:17 +0700

    Add files via upload

---
+++ team/havid_424.py
@@ -0,0 +1,10 @@
+def perkenalan(nama424, nim424, peran424):
+    print("Yo! Perkenalkan, Nama ku", nama424,"!")
+    print("NIM : ", nim424)
+    print("Peran saya yaitu : ", peran424)
+
+if __name__ == "__main__":
+    nama424 = "Havid Danggo  Pamungkas"
+    nim424 = "2213010424"
+    peran424 = "Membuat Tampilan Bagian Artikel"
+    perkenalan(nama424, nim424, peran424)
