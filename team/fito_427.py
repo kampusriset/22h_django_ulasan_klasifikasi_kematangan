402656dff53942acfae069fdffdc668562fb5f45
fpr35 <fitopatria@gmail.com>
2025-02-20 14:50:55 +0700

    Add files via upload

---
+++ team/fito_427.py
@@ -0,0 +1,10 @@
+def perkenalan(nama427, nim427, peran427):
+    print("Yo! Perkenalkan, Nama ku", nama427,"!")
+    print("NIM : ", nim427)
+    print("Peran saya yaitu : ", peran427)
+
+if __name__ == "__main__":
+    nama427 = "Fito Patria"
+    nim427 = "2213010427"
+    peran427 = "Membuat Fitur Machine Learning, CRUD, dan semua yang berhubungan dengan backend"
+    perkenalan(nama427, nim427, peran427)
