2678ce54d5d46dedc893435892f4a3df11b7073b
raflesia11 <raflesiajuliyanto@gmail.com>
2025-02-20 15:06:02 +0700

    Add files via upload

---
+++ team/Mukhlis_428.py
@@ -0,0 +1,10 @@
+def perkenalan(nama428, nim428, peran428):
+    print("Hai Perkenalkan, Nama ku", nama428)
+    print("NIM : ", nim428)
+    print("Peran saya yaitu : ", peran428)
+
+if __name__ == "__main__":
+    nama428 = "Raflesia Mukhlis Juliyanto"
+    nim428 = "2213010428"
+    peran428 = "Membuat Tampilan Bagian About"
+    perkenalan(nama428, nim428, peran428)
