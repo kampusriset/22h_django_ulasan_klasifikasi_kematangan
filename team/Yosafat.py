884829183433f411529406a2aa828d0937a4cc26
Yosafat441 <gerriyosafat@gmail.com>
2025-02-20 18:29:51 +0700

    Add files via upload

---
+++ team/Yosafat.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if __name__ == "__main__":
+    nama = "Yosafat Gerrimandaril"
+    nim = "2213010441"
+    peran = "Membuat tampilan bagian Footer"
+    perkenalan(nama, nim, peran)
